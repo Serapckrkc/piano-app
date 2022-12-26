d42aad328da1f3ec0f39875bba1b8caa2e894a34
Serap Acıpayam <Serapckrkc@gmail.com>
2022-12-26 14:22:47 +0300

    variables defined and audios added

---
+++ app.js
@@ -0,0 +1,3 @@
+const containerKeys = document.querySelectorAll(".container-keys .key")
+const containerVolumeSlider = document.querySelector(".container-header-volume-slider input")
+const containerCheckbox = document.querySelector(".container-header-keys-checkbox input")
